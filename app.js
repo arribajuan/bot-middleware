f2ccc05f6126e8a1d1e08b6ada24c93400100f3f
Juan Martinez <arribajuan@gmail.com>
2019-01-30 21:27:45 -0500

    chagnes to hello world

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// HELLO WORLD - DID NOT WORK IN AZURE
+// const express = require('express')
+// const app = express()
+// const port = process.env.PORT || 3000
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+// SOME OTHER HELLO WORLD ... 
+const http = require('http');
+const port=process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Hello World</h1>');
+});
+
+server.listen(port,() => {
+    console.log(`Server running at port `+port);
+});
